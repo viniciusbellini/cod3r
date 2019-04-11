7dda5eaa292bc93f27f53d034536324b52b12141
Vinicius Bellini <viniciusbellini@gmail.com>
2019-04-11 11:00:43 -0300

    hoisting, notacaoPonto, objeto2

---
+++ exercicios-js/fundamentos/notacaoPonto.js
@@ -0,0 +1,12 @@
+console.log(Math.ceil(6.1))
+
+const obj1 = {}
+obj1.nome = 'Bola'
+console.log(obj1.nome)
+
+function Obj(nome){
+    this.nome = nome
+}
+
+const obj2 = new Obj('Cadeira')
+console.log(obj2)
---
+++ exercicios-js/fundamentos/objeto2.js
@@ -0,0 +1,6 @@
+console.log('Object -', typeof Object)
+console.log('Object -', typeof new Object)
+
+const Cliente = function() {}
+console.log('Cliente -', typeof Cliente)
+console.log('Cliente -', typeof new Cliente)
